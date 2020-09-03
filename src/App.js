d451e34d2970d0c98c92d06aa654bc30447fb86d
roninJosue <roninjosue88@gmail.com>
2020-09-03 08:18:29 -0600

    Applying Js Styles

--- src/App.js
+++ src/App.js
@@ -1,61 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import tasks from './sample/task.json';
 
-/* function HelloWorld(props) {
-  return (
-    <div id="hello">
-      <h3>{props.lastname}</h3>
-      <h4>{props.name}</h4>
-    </div>
-  );
-} */
+import Tasks from './components/Tasks';
 
-class HelloWorld extends React.Component {
+class App extends Component {
 
   state = {
-    show: true
-  };
-
-  toggleShow = () => {
-    this.setState({show: !this.state.show})
+    tasks
   }
 
-  render(){
-    if(this.state.show){
-      return (
-        <div id="hello">
-        <h3>{this.props.lastname}</h3>
-        <h4>{this.props.name}</h4>
-        <button onClick={this.toggleShow}>Toggle show</button>
+  render() {
+    return(
+      <div>
+        <Tasks tasks={this.state.tasks} />
       </div>
-      );
-    } else {
-      return <h1>
-        There are not elements.
-        <button onClick={this.toggleShow}>Toggle show</button>
-        </h1>
-    }
+    )
   }
-}
-
-//const App = () => <div>My component: <HelloWorld /></div>
-
-/*class App extends React.Component {
-  render(){
-    return (
-      <div>My component: <HelloWorld /></div>
-    );
-  }
-}*/
-
-function App() {
-  return (
-    <div>
-      My component: <HelloWorld name="Hello Reynaldo" lastname="Cano" />
-      <HelloWorld name="Hello Roxana" />
-      <HelloWorld name="Hello Eli" />
-    </div>
-  );
 }
 
 export default App;
