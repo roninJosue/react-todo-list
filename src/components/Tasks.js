2b63301f9959df6607ac541b6a43b02fb835a996
roninJosue <roninjosue88@gmail.com>
2020-09-07 12:38:10 -0600

    Adding Task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from  'prop-types'
 
 import Task from './Task';
 
@@ -7,6 +8,10 @@ export class Tasks extends Component {
         return  this.props.tasks.map((task) => <Task task={task} key={task.id} />)
         
     }
+}
+
+Tasks.propTypes = {
+    tasks: PropTypes.array.isRequired
 }
 
 export default Tasks
